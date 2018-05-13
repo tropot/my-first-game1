15addd8c3f4b49d604da821329d763a3aea83e01
Sergio <mangustar123@gmail.com>
2018-05-13 13:26:34 +0300

    am adaugat tot de la oeneemyOne in programa

--- game.js
+++ game.js
@@ -36,9 +36,9 @@ function enemy2Move() {
 function checkEnd(xe,ye) {
   //console.log("xe :",xe + el)
 var xes = xe - player.l
-var xed = xe + el
+var xed = xe + langth
   if(player.x > xes && player.x < xed && player.y < e1y+140 || player.y < enemyTwo.y+140) {
-      e1y = 10
+      enemyOne.y = 10
       enemyTwo.y = 10
       time = 0
       console.log("xes",xes,"xed",xed,"pX",player.x,"Y",ye)
@@ -48,15 +48,15 @@ var xed = xe + el
 }
 function enemy1Move() {
   //console.log("before  enemy1Move ", e1y)
-  e1y += 5
+  enemyOne.y += 5
   //console.log(" enemy1Move ", e1y)
-  if (e1y >= 625) {
-  e1y = 10
+  if (enemyOne.y >= 625) {
+  enemyOne.y = 10
   }
 }
 function ciclu() {
   //console.log("in ciclu");
-  checkEnd(e1x,e1y)
+  checkEnd(enemyOne.x,enemyOne.y)
   checkEnd(enemyTwo.x,enemyTwo.y)
   backround();
   Theplayer();
@@ -81,7 +81,7 @@ function Theplayer(){
     }
 function enemy1() {
   cc.fillStyle='gray'
-  cc.fillRect(e1x,e1y,langth,langth)
+  cc.fillRect(enemyOne.x,enemyOne.y,langth,langth)
     }
 function enemy2() {
   cc.fillStyle='gray'
